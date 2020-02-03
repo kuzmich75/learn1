ddaf190f9a072978f862b9dc2797fbd668aebd40
Kuzmich <dima.kazanin@yandex.ru>
2020-02-03 22:16:48 +0300

    add two functions

--- budget-001.js
+++ budget-001.js
@@ -1,30 +1,47 @@
 'use strict';
-let money = +prompt("Ваш бюджет на месяц?", "20000"),
-    time = prompt("Введите дату в формате YYYY-MM-DD", "1975-02-16"),
-    appData = {
+let money, time;
+
+function start(){
+    money = +prompt("Ваш бюджет на месяц?", "20000");
+    time = prompt("Введите дату в формате YYYY-MM-DD", "1975-02-16");
+
+    while(isNaN(money) || money == "" || money == null) {
+        money = +prompt("Ваш бюджет на месяц?", "20000");
+    }
+}
+start();
+
+   let appData = {
     budget: money, 
     date: time, 
     expenses: {},
     optionalExpenses: {},
     income: [],
-    savings: false,
+    savings: true,
 };
 
-for (let i=0; i<2; i++) {
-    let expenseItem = prompt("Введите обязательную статью расходов в этом месяце", ""),
-        itCost = prompt("Во сколько обойдется?", "");
-
-if ( (typeof(expenseItem))=== 'string' && (typeof(expenseItem)) != null  && (typeof(itCost)) != null 
-      && itCost != '' && expenseItem != '' && expenseItem.length < 40 ){
-        alert('done');
-    appData.expenses[expenseItem] = itCost;
+function chooseExpenses(){
+    for (let i=0; i<2; i++) {
+        let expenseItem = prompt("Введите обязательную статью расходов в этом месяце", ""),
+            itCost = +prompt("Во сколько обойдется?", "");
     
-}else {
-alert('Некорректные данные');
+    if ( (typeof(expenseItem))=== 'string' && (typeof(expenseItem)) != null  && (typeof(itCost)) != null
+     && itCost != '' && expenseItem != '' && expenseItem.length < 40 ) {
+            alert('done');
+            console.log("done");
+        appData.expenses[expenseItem] = itCost;
+        
+    }else {
+    alert('Некорректные данные');
+    i = i - 1;
+        }
+    }
 }
-};
 
-appData.moneyPerDay = appData.budget/30;
+chooseExpenses();
+
+
+appData.moneyPerDay = (appData.budget/30).toFixed(2);
     alert('Ежедневный бюджет: ' + appData.moneyPerDay);
 
     if(appData.moneyPerDay < 1000){
@@ -39,3 +56,14 @@ appData.moneyPerDay = appData.budget/30;
     else{
         alert('Что-то пошло не так');
     }
+
+    function checkSavings(){
+        if (appData.savings == true){
+            let save = +prompt("Какова сумма вклада"),
+                percent = +prompt("Каков процент по вкладу");
+
+                appData.monthIncome = save/100/12*percent;
+                alert("Ваш доход по депозиту в месяц: " +appData.monthIncome);
+        }
+    }
+    checkSavings();  
